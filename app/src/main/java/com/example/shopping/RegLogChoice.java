629157cb0b0d134c277cd21c6039611a22f11224
97059 <engislamustafa@gmail.com>
2022-06-05 13:57:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shopping/RegLogChoice.java
@@ -0,0 +1,45 @@
+package com.example.shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegLogChoice extends AppCompatActivity {
+    Button reg,log;
+    TextView admlog,stflog,about;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_log_choice);
+        reg=(Button)findViewById(R.id.btnreg);
+        log=(Button)findViewById(R.id.btnlog);
+
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegLogChoice.this,LoginPage.class);
+                startActivity(intent);
+            }
+        });
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegLogChoice.this,RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+    public void onBackPressed(){
+     moveTaskToBack(true);
+     android.os.Process.killProcess(android.os.Process.myPid());
+     System.exit(1);
+    }
+}
